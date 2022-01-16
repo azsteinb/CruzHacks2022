b39f790c9a4aeca93b690ae909faae629bee62ae
aaron <aaron@fishtank.attlocal.net>
2022-01-16 08:43:53 -0800

    distr

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,7 @@
 <template>
   <header class="has-background-primary p-4">
-    <h1 class="title has-text-white">Health Hacker</h1>
+    <h1 class="title has-text-white">Medibill</h1>
+    
   </header>
+
 </template>
