94a5dd0692e2e886e4274ed77144f6064655968a
aaron <aaron@fishtank.attlocal.net>
2022-01-16 04:10:55 -0800

    Here is what is working: You can upload a file, and the site will succesfully call our cloud scripts based off the pdf dataa. Now we just have to finish the frontend and integrate stuff

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -22,7 +22,7 @@
                         </span>
                     </label>
                 </div>
-                <button id='file-submit' class="button is-primary ml-2" v-on:click="handleFile()">Cick Me To Discover Your Savings!</button>
+                <button id='file-submit' class="button is-primary ml-2" v-on:click="fileButtonPress()">Cick Me To Discover Your Savings!</button>
             </div>
         </div>
     </div>
@@ -32,6 +32,7 @@
 
 <script>
     import AppTable from './AppTable.vue'
+    import axios from 'axios'
 
     const getFile = () => {
         return document.querySelector('.file-input').files.item(0);
@@ -44,7 +45,7 @@
     const getNumber = (num) => {
         return num;
     }
-    const handleFile = () => {
+    /* const handleFile = () => {
         document.querySelector('#content').classList.add('is-hidden');
         document.querySelector('#table-container').classList.remove('is-hidden');
         const data = [['80050', 'General health panel', '$201', '0'], 
@@ -56,8 +57,70 @@
         console.log(data);
         //Parse PDF grabbing file with getFile() to get data
         generateTableRows(data);
+    } */
+
+    function getDescription(code) {
+        let url = "https://us-central1-cruzhacks2022-338309.cloudfunctions.net/cpt_decription?code=" + code;
+        return new Promise(function (resolve, reject) {
+            axios.get(url).then(
+                (response) => {
+                    var result = response.data;
+                    resolve(result);
+                },
+                    (error) => {
+                    reject(error);
+                }
+            );
+        });
     }
+    function getDict(encodedPDF){
+        let url = "https://us-central1-cruzhacks2022-338309.cloudfunctions.net/parsePDF";
+        return new Promise(function (resolve, reject) {
+            axios.post(url, { "pdf-data": encodedPDF }).then(response => {
+                var result = response.data;
+                resolve(result);
+            }, 
+                (error) => {
+                    reject(error);
+                })
+        });
+    }
+
+    async function fileButtonPress(){
+        document.querySelector('#content').classList.add('is-hidden');
+        // show loading screen
+
+        // We need to parse the pdf
+        // We need to get the file that the user inputted
+        console.log(getFile())
+
+        var reader = new FileReader();
+        //var codePrices = {}
+        let res = null;
+        reader.onload=function(){
+            res = btoa(reader.result);
+            //console.log(res);
+            //console.log(await getDict2(res));
+            console.log(getDict(res));
+        }
+        
+        reader.readAsBinaryString(getFile());
+
+        //console.log(await getDict2(res));
+
+        const data = [['80050', await getDescription('84439'), '$201', '0'], 
+                        ['84439', await getDescription('84439'), '$55', '0'], 
+                        ['83206', await getDescription('84439'), '$207', 0], 
+                        ['80061', await getDescription('84439'), '$80', getNumber(0)], 
+                        ['99396', await getDescription('84439'), '$455', getNumber(5)]
+                    ];
+        generateTableRows(data);
+        
+        document.querySelector('#table-container').classList.remove('is-hidden');
+    }
+
     function generateTableRows(data) {
+        
         const tableBody = document.querySelector('#tableBody');
         data.forEach((item) => {
             const row = document.createElement('tr');
@@ -81,7 +144,7 @@
         methods: {
             getFile,
             setFile,
-            handleFile,
+            fileButtonPress,
             generateTableRows,
         }
     }
